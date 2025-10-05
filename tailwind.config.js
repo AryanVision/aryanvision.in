b07e822f36ad789567bf27e5500e79cd2f4a39ba
AryanVision <aryanvision.portal@gmail.com>
2025-10-05 20:18:16 +0530

    Add files via upload
    Initial Commit

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  content: ["./pages/**/*.{js,jsx}", "./components/**/*.{js,jsx}"],
+  theme: {
+    extend: {
+      colors: {
+        saffron: "#FF6F00",
+        indigo: "#283593",
+        ivory: "#FAFAFA",
+        ash: "#757575",
+        ggreen: "#4CAF50"
+      }
+    }
+  },
+  plugins: []
+}
